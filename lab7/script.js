4c3db57f1d08273ca34545ebb7fa87e5c138054a
tusxd <tusxdie@gmail.com>
2023-10-02 12:14:32 +0700

    Lab7

---
+++ lab7/script.js
@@ -0,0 +1,15 @@
+console.log("BRUH!")
+
+const a = 1;
+const b = 2;
+
+console.log("a=" + a);
+console.log("b=" + b);
+console.log("a + b = " + (a + b));
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
